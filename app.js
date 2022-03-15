1a4d388895f99b2f50090e1b75cb591f040553c0
Joaopcamposs <joaopcamposs@outlook.com>
2022-03-15 17:45:59 -0300

    Adicionando persistencia de dados com arquivos

--- app.js
+++ app.js
@@ -1,11 +1,20 @@
 const express = require("express");
 const { randomUUID } = require("crypto");
+const fs = require("fs");
 
 const app = express(); 
 
 app.use(express.json());
 
-const products = [];
+let products = [];
+
+fs.readFile("products.json", "utf-8", (err, data) => {
+    if(err){
+        console.log(data);
+    }else{
+        products = JSON.parse(data);
+    }
+});
 
 app.post("/products", (request, response) => {
     // Nome e Preco => name e price
@@ -16,9 +25,11 @@ app.post("/products", (request, response) => {
         name,
         price,
         id: randomUUID(),
-    }
+    };
 
     products.push(product);
+
+    productFile();
 
     return response.json(product);
     //console.log(body);
@@ -45,6 +56,8 @@ app.put("/products/:id", (request, response) => {
         price
     }
 
+    productFile();
+
     return response.json({message: "Produto alterado com sucesso!"});
 });
 
@@ -55,7 +68,19 @@ app.delete("/products/:id", (request, response) => {
         
     products.splice(productIndex, 1);
 
+    productFile();
+
     return response.json({message: "Produto removido com sucesso!"});
 })
+
+function productFile(){
+    fs.writeFile("products.json", JSON.stringify(products), (err) => {
+        if(err) {
+            console.log(err);
+        }else{
+            console.log("Produto inserido!");
+        }
+    });
+}
 
 app.listen(4002, () => console.log("Servidor esta rodando na porta 4002"));
