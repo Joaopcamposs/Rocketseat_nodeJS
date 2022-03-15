ebd63d22edb44f839b8e19ee7c4efa47cac25dc4
Joaopcamposs <joaopcamposs@outlook.com>
2022-03-15 16:36:21 -0300

    Rota POST concluida, insercao de objeto

--- app.js
+++ app.js
@@ -1,15 +1,27 @@
 const express = require("express");
+const { randomUUID } = require("crypto")
 
-const app = express();
+const app = express(); 
+
+app.use(express.json());
 
 const products = [];
 
 app.post("/products", (request, response) => {
     // Nome e Preco => name e price
 
-    const body = request.body;
+    const {name , price} = request.body;
 
-    console.log(body);
+    const product = {
+        name,
+        price,
+        id: randomUUID(),
+    }
+
+    products.push(product);
+
+    return response.json(product);
+    //console.log(body);
 });
 
 app.listen(4002, () => console.log("Servidor esta rodando na porta 4002"));
